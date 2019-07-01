8c4c890f7eb5a0970196e45ae83e19b0e631916d
haqqer <ilhamserbaguna@gmail.com>
2019-07-01 15:10:00 +0700

    update baseUrl

--- components/daftar/step-2.vue
+++ components/daftar/step-2.vue
@@ -245,7 +245,7 @@ export default {
         { id: 0, name: 'male', alias: 'Laki-Laki' },
         { id: 1, name: 'female', alias: 'Perempuan' }
       ],
-      baseURL: 'http://35.247.145.161:3200/api'
+      baseURL: 'https://api.futureleadersummit.org/api'
     }
   },
   computed: {
