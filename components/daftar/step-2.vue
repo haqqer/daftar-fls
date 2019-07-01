c0c0a8819d5bd4a1a5fde96882fdf4760cdad397
haqqer <ilhamserbaguna@gmail.com>
2019-07-01 09:53:50 +0700

    prepare deploy on netlify

--- components/daftar/step-2.vue
+++ components/daftar/step-2.vue
@@ -245,7 +245,7 @@ export default {
         { id: 0, name: 'male', alias: 'Laki-Laki' },
         { id: 1, name: 'female', alias: 'Perempuan' }
       ],
-      baseURL: 'http://localhost:9000/api'
+      baseURL: 'http://35.247.145.161:3200/api'
     }
   },
   computed: {
